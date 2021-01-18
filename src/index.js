68d04eb2096efc7c2a98489d918fc4a09290c568
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-01-18 19:46:46 +0330

    e10- first component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const element = React.createElement(
-     "div",
-     { id: "main", className: "container" },
-     "hello world!"
-);
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
