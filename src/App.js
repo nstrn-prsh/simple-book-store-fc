a1644f9e5fb6341fe48467017eb8b0b67385e0e1
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-01-19 01:19:40 +0330

    e16

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
-import Product from './Component/Product/Product';
-
-const App = () => {
-     return(
-          <div className="container">
-              <h2>React App</h2>
-              <p>Hello World!</p>
-              <Product />
-          </div>
-     );
-};
+import React, { Component } from "react";
+import Product from "./Component/Product/Product";
+class App extends Component {
+     render() {
+          return (
+               <div className='container'>
+                    <h2>React App</h2>
+                    <p>Hello World!</p>
+                    <Product title="cheese 2" price="200" />
+                    <Product title="cheese 3" price="300" >
+                        Discount: 20%
+                    </Product>
+                    <Product title="cheese 4" price="400" />
+               </div>
+          );
+     }
+}
 
 export default App;
--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Product = () => {
+const Product = (props) => {
+    const name= 'book'
     return ( 
         <div>
-            <h3>it is a product.</h3>
+            <h3>it is a product {name}.</h3>
+            <h5>name: {props.title}.</h5>
+            <h5>price: {props.price}.</h5>
+            <h5>{props.children}</h5>
         </div>
      );
 }
