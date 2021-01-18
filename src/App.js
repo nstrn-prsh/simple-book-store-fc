2b61f659756209956cbef58e4a892d979ea55806
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-01-18 20:10:04 +0330

    e12-another component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Product from './Component/Product/Product';
 
 const App = () => {
      return(
           <div className="container">
               <h2>React App</h2>
               <p>Hello World!</p>
+              <Product />
           </div>
      );
 };
---
+++ src/Component/Product/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+    return ( 
+        <div>
+            <h3>it is a product.</h3>
+        </div>
+     );
+}
+ 
+export default Product;
