3dd8b6cb2ffb7aa175d1e57389fa30bf2ec0f607
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-01-19 14:28:41 +0330

    e21

--- src/App.js
+++ src/App.js
@@ -1,96 +1,96 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Product from "./Component/Product/Product";
 
-const App = () => {
-     const [productState, setProductState] = useState({
+class App extends Component {
+     state = {
           products: [
                { title: "Book 1", price: 100 },
                { title: "Book 2", price: 200 },
                { title: "Book 3", price: 300 },
           ],
-     });
-     const changePriceHandler = () => {
-          setProductState({
+     };
+
+     changePriceHandler = () => {
+          console.log("this is working!");
+          this.setState({
                products: [
                     { title: "Book 1", price: 10 },
-                    { title: "Book 2", price: 10 },
-                    { title: "Book 3", price: 10 },
+                    { title: "Book 2", price: 20 },
+                    { title: "Book 3", price: 30 },
                ],
           });
      };
 
-     return (
-          <div className='container'>
-               <h2>React App</h2>
-               <p>Hello World!</p>
-               <Product
-                    title={productState.products[0].title}
-                    price={productState.products[0].price}
-               />
-               <Product
-                    title={productState.products[1].title}
-                    price={productState.products[1].price}
-               >
-                    Discount: 20%
-               </Product>
-               <Product
-                    title={productState.products[2].title}
-                    price={productState.products[2].price}
-               />
+     render() {
+          return (
+               <div className='container'>
+                    <h2>React App</h2>
+                    <p>Hello World!</p>
+                    <Product
+                         title={this.state.products[0].title}
+                         price={this.state.products[0].price}
+                    />
+                    <Product
+                         title={this.state.products[1].title}
+                         price={this.state.products[1].price}
+                    >
+                         Discount: 20%
+                    </Product>
+                    <Product
+                         title={this.state.products[2].title}
+                         price={this.state.products[2].price}
+                    />
 
-               <button onClick={changePriceHandler}>click on me!</button>
-          </div>
-     );
-};
+                    <button onClick={this.changePriceHandler}>
+                         click on me!
+                    </button>
+               </div>
+          );
+     }
+}
 
 export default App;
 
-// class App extends Component {
-//      state = {
+// const App = () => {
+//      const [productState, setProductState] = useState({
 //           products: [
 //                { title: "Book 1", price: 100 },
 //                { title: "Book 2", price: 200 },
 //                { title: "Book 3", price: 300 },
 //           ],
-//      };
-
-//      changePriceHandler = () => {
-//           console.log("this is working!");
-//           this.setState({
+//      });
+//      const changePriceHandler = () => {
+//           setProductState({
 //                products: [
 //                     { title: "Book 1", price: 10 },
-//                     { title: "Book 2", price: 20 },
-//                     { title: "Book 3", price: 30 },
+//                     { title: "Book 2", price: 10 },
+//                     { title: "Book 3", price: 10 },
 //                ],
 //           });
 //      };
 
-//      render() {
-//           return (
-//                <div className='container'>
-//                     <h2>React App</h2>
-//                     <p>Hello World!</p>
-//                     <Product
-//                          title={this.state.products[0].title}
-//                          price={this.state.products[0].price}
-//                     />
-//                     <Product
-//                          title={this.state.products[1].title}
-//                          price={this.state.products[1].price}
-//                     >
-//                          Discount: 20%
-//                     </Product>
-//                     <Product
-//                          title={this.state.products[2].title}
-//                          price={this.state.products[2].price}
-//                     />
+//      return (
+//           <div className='container'>
+//                <h2>React App</h2>
+//                <p>Hello World!</p>
+//                <Product
+//                     title={productState.products[0].title}
+//                     price={productState.products[0].price}
+//                />
+//                <Product
+//                     title={productState.products[1].title}
+//                     price={productState.products[1].price}
+//                >
+//                     Discount: 20%
+//                </Product>
+//                <Product
+//                     title={productState.products[2].title}
+//                     price={productState.products[2].price}
+//                />
 
-//                     <button onClick={this.changePriceHandler}>
-//                          click on me!
-//                     </button>
-//                </div>
-//           );
-//      }
-// }
+//                <button onClick={changePriceHandler}>click on me!</button>
+//           </div>
+//      );
+// };
 
 // export default App;
