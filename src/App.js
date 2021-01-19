27c22df809adf26a22b1feaa5e03a34af0c33b46
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-01-19 12:40:32 +0330

    e17

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import Product from "./Component/Product/Product";
 class App extends Component {
+     state = {
+          products: [
+               { title: "Book 1", price: 100 },
+               { title: "Book 2", price: 200 },
+               { title: "Book 3", price: 300 },
+          ],
+     };
      render() {
           return (
                <div className='container'>
                     <h2>React App</h2>
                     <p>Hello World!</p>
-                    <Product title="cheese 2" price="200" />
-                    <Product title="cheese 3" price="300" >
-                        Discount: 20%
+                    <Product
+                         title={this.state.products[0].title}
+                         price={this.state.products[0].price}
+                    />
+                    <Product
+                         title={this.state.products[1].title}
+                         price={this.state.products[1].price}
+                    >
+                         Discount: 20%
                     </Product>
-                    <Product title="cheese 4" price="400" />
+                    <Product
+                         title={this.state.products[2].title}
+                         price={this.state.products[2].price}
+                    />
                </div>
           );
      }
