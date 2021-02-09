87d3202c947964c5e92269a1defcdcc167a9ba95
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-02-09 18:55:58 +0330

    e115

--- src/Components/Shopping/Account.js
+++ src/Components/Shopping/Account.js
@@ -1,9 +1,19 @@
-import React, { Component}  from 'react'
+import React, { Component } from "react";
+import "./../componentCss/account.css";
 
 class Account extends Component {
-    render() { 
-        return ( <h2>user Account</h2> );
-    }
+     render() {
+          return (
+               <div className='account'>
+                    <form>
+                         <input type='text' placeholder='full name...' />
+                         <input type='email' placeholder='email...' />
+                         <input type='password' placeholder='password...' />
+                         <button>submit</button>
+                    </form>
+               </div>
+          );
+     }
 }
- 
+
 export default Account;
